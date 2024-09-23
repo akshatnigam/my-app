77259889c2175914a27f7dd87cb6493a33f8a897
Akshat Nigam <Akshat.Nigam3@T-Mobile.com>
2024-09-23 14:47:24 +0530

    Adding more Logic to TextUtils | Complete React Course in Hindi #8

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 export default function Navbar({ title = 'Set Title here', aboutText = 'About Us' }) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">{title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,7 +21,7 @@ export default function Navbar({ title = 'Set Title here', aboutText = 'About Us
           </ul>
           <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
+            <button className="btn btn-outline-primary" type="submit">Search</button>
           </form>
         </div>
       </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,18 +12,34 @@ export default function TextForm({pageTitle= 'Enter your title here:', textBoxTi
         // console.log("On change");
         setText(event.target.value);
     }
-    const [text, setText] = useState("Enter Text Here");
+    const handleLoClick = ()=>{
+        //console.log("LowerCase was Clicked" + text)
+        let newtext = text.toLocaleLowerCase();
+        setText(newtext);
+    }
+    const [text, setText] = useState("");
 // text = "new Text"; wrong way to change the state
 // setText("Hello World"); correct way to change the state
   return (
     <>
+    <div className="container my-3">
         <h1>{pageTitle}</h1>
             <div className="form-floating my-3">
-                <textarea className="form-control" value={text} onChange = {handleOnChange} id="textBox" style={{height: "400px"}} ></textarea>
+                <textarea className="form-control" value={text} onChange = {handleOnChange} id="textBox" style={{height: "200px"}} ></textarea>
                 <label htmlFor="textBox">{textBoxTitle}</label>
             </div>
-            <button type="button" className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-            </>
+                <button type="button" className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button type="button" className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+
+            </div>
+    <div className='container my-4'>
+        <h2>Your Text Summary:</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008* text.split(" ").length} Minutes read</p>
+        <h2>Preview:</h2>
+        <p>{text}</p>
+    </div>
+    </>
   );
 }
 // pts -> PropTypes.string
